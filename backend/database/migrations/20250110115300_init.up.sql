b5b6f43e38e61a160c519f3697c4b3bbb6adeb6b
Tom Härter <tomuhaerter@gmail.com>
2025-01-17 09:46:53 +0100

    feat: gitignore and formatting

--- backend/database/migrations/20250110115300_init.up.sql
+++ backend/database/migrations/20250110115300_init.up.sql
@@ -1,8 +1,9 @@
-CREATE TABLE papers (
-    id uuid PRIMARY KEY,
-    title text NOT NULL,
-    authors text NOT NULL,
-    file_url text NOT NULL,
-    file_hash text NOT NULL,
+CREATE TABLE papers
+(
+    id         uuid PRIMARY KEY,
+    title      text        NOT NULL,
+    authors    text        NOT NULL,
+    file_url   text        NOT NULL,
+    file_hash  text        NOT NULL,
     created_at timestamptz NOT NULL DEFAULT now()
 );
--- backend/database/test_database.py
+++ backend/database/test_database.py
@@ -1,13 +1,12 @@
 import psycopg
 
 
-with psycopg.connect("dbname=LLMsForScience user=root host=localhost password=root port=5432") as conn:
-    
+with psycopg.connect(
+    "dbname=postgres user=postgres host=localhost password=postgres port=5432"
+) as conn:
     with conn.cursor() as cur:
-        
-        cur.execute("""
-                    SELECT * FROM pg_catalog.pg_tables""")
-        
+        cur.execute("SELECT * FROM pg_catalog.pg_tables")
+
         fetched = cur.fetchall()
-        
+
         print(fetched)
