db4e7e8574aaa1fcf4814c7d5c15b859af027475
Antim8 <petersen.tim@outlook.de>
2025-08-23 08:28:57 +0200

    chore: UV update and migrations

--- server/utils/drizzle.ts
+++ server/utils/drizzle.ts
@@ -2,7 +2,10 @@ import { drizzle } from "drizzle-orm/postgres-js"
 import * as schema from "~~/packages/database/schema"
 import postgres from "postgres"
 
-const client = postgres(process.env.POSTGRES_URL!)
+
+const POSTGRES_URL = process.env.POSTGRES_URL!
+console.log("POSTGRES_URL:", POSTGRES_URL)
+const client = postgres(POSTGRES_URL)
 
 const db = drizzle({ client, schema, casing: "snake_case" })
 
